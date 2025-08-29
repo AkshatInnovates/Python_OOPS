72a2f01ddcc10765f8800df7b58cc623e9342a99
Akshat Jain <145150367+AkshatInnovates@users.noreply.github.com>
2025-08-29 12:01:05 +0000

    Class and object Basics Done

---
+++ 01_Basic Class and Object.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+
+my_car = Car('Inova','Crysta')
+my_car2 = Car('Toyota','Corolla')
+print(my_car.brand)
+print(my_car.model)
+print(my_car2.brand)
+print(my_car2.model)
